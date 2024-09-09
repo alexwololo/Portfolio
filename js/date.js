45dd42d11c1f039df7003993094b1a07c54cc668
alexwololo <alexander.caspersson@gmail.com>
2024-09-09 09:39:47 +0200

    updated date to show date dynamiccly instead of hardcoded date

--- js/date.js
+++ js/date.js
@@ -1,7 +1,6 @@
 function showDate() {
   let today = new Date();
   let day = today.getDate();
-
   let months = [
     'January',
     'February',
@@ -16,18 +15,16 @@ function showDate() {
     'November',
     'December',
   ];
-
   let month = months[today.getMonth()];
 
   const dateElement = document.getElementById('dateDisplay');
   dateElement.innerHTML = `
     <div class="calendar">
-    <div class="handles"></div>
-    <div class="month">August</div>
-    <div class="separator"></div>
-    <div class="day">24</div>
-</div>
-
+      <div class="handles"></div>
+      <div class="month">${month}</div>
+      <div class="separator"></div>
+      <div class="day">${day}</div>
+    </div>
   `;
 }
 
